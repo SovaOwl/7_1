d7b09da6832328073dc9e3382126957846ed4f1c
SovaOwl <sova27755@gmail.com>
2022-07-27 20:15:24 +0300

    First commit

---
+++ 7_1/7_1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#define MODE 3
+
+#ifndef MODE
+#error Необходимо определить MODE
+#endif // !MODE
+
+#if MODE == 1
+int add(int x, int y)
+{
+	return x + y;
+}
+#endif // !1
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int num1 = -999, num2 = -999;
+
+#if MODE == 0
+	cout << "Работаю в режиме тренировки" << endl;
+
+#elif MODE == 1
+	cout << "Работаю в боевом режиме" << endl;
+	cout << "Введите число 1: ";
+	cin >> num1;
+	cout << "Введите число 2: ";
+	cin >> num2;
+	cout << "Результат сложения: " << add(num1, num2);
+
+#else
+	cout << "Неизвестный режим. Завершение работы" << endl;
+#endif
+}
